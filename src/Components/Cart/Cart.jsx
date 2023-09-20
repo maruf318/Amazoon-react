91ccad538fa86d6956f70e6373f3cb50903f43de
Maruf Hossain <hossainmaruf001@gmail.com>
2023-09-20 02:00:46 +0200

    useNavigate used in product cart to navigate back home

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,14 +1,18 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const product = useLoaderData();
-  console.log(product);
+  const navigate = useNavigate();
+  // console.log(product);
   const { id, title, description, price, brand, thumbnail } = product;
+  const handleHome = () => {
+    navigate("/");
+  };
   return (
     <div>
       <a
         href="#"
-        className="flex  items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        className="flex m-5  items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
       >
         <img
           className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
@@ -27,6 +31,12 @@ const Cart = () => {
           <p className="text-3xl font-bold">Price: $ {price}</p>
           <button className="bg-red-400 p-4 m-2 rounded-lg font-bold text-white">
             Add to cart
+          </button>
+          <button
+            onClick={handleHome}
+            className="bg-slate-300 p-4 rounded-lg m-6"
+          >
+            Home
           </button>
         </div>
       </a>
